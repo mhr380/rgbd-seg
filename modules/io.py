9123fcf05a4b4a5afe194c7ba422ac40881d2103
0000143145 <hajime.mihara@sony.com>
2018-03-17 17:01:34 +0900

    implemented

--- modules/io.py
+++ modules/io.py
@@ -13,53 +13,112 @@ import os
 import cv2
 import numpy as np
 
-
-def load_depth(filename, depth_max=1.0):
-    ''' loads 16bit depth image and
-        returns depth as 1ch np.float32 array.
-
-        @Args:
-            filename: path to depth image
-            depth_max: max value of depth
-        @Returns
-            depth: np.float32 array.
-    '''
-
-    bit16 = 65535
-
-    try:
-        depth_16bit = cv2.imread(filename, -1)
-    except:
-        print('Load Error: {}'.format(filename))
-        sys.exit()
-
-    depth = np.float32(depth_16bit) / float(bit16) * depth_max
-
-    return depth
+from keras.preprocessing.image import load_img, img_to_array
 
 
-def load_image(filename, image_max=1.0):
+class ImageDataGenerator(object):
+    def __init__(self):
+        self.__bit16 = 65535
+        self.__bit8 = 65535
 
-    bit8 = 255
+        self.dirname = None
+        self.reset()
 
-    try:
-        image_8bit = cv2.imread(filename, 1)
-    except:
-        print('Load Error: {}'.format(filename))
-        sys.exit()
+    def reset(self):
+        self.images = []
+        self.labels = []
 
-    image = np.float32(image_8bit) / float(bit8) * image_max
+    def flow_from_directory(self, dirname, batch_size=32):
 
-    return image
+        self.dirname = dirname
+
+        out_list = []
+        for state in ['train', 'test']:
+            image_names = os.listdir(
+                os.path.join(self.dirname, state, 'color')
+                )
+
+            num_amples = len(image_names)
+
+            # make [[color_name, depth_name], label_name] list
+            data_path_list = []
+            for image_name in image_names:
+                color_path = os.path.join(self.dirname, 'train', 'color')
+                depth_path = os.path.join(self.dirname, 'train', 'depth')
+
+                label_path = os.path.join(self.dirname, 'train', 'label')
+
+                data_path_list.append(
+                    ([color_path, depth_path], label_path))
+
+            # random shuffle
+            data_path_list = np.random.permutation(data_path_list)
+
+            x_list = []
+            y_list = []
+
+            for (imagedepth_path, label_path) in data_path_list:
+                image_path = imagedepth_path[0]
+                depth_path = imagedepth_path[1]
+
+                image = self.__load_image(image_path)
+                depth = self.__load_image(depth_path)
+
+                label = self.__load_image(label_path)
+
+                # make 4ch array (B, G, R, D)
+                x = np.dstack((image, depth))
+                x_list.append(x)
+                y_list.append(label)
+
+            out_list.append((x_list, y_list))
+
+        return out_list
+
+    def __load_depth(self, filename, depth_max=1.0):
+        ''' loads 16bit depth image and
+            returns depth as 1ch np.float32 array.
+
+            @Args:
+                filename: path to depth image
+                depth_max: max value of depth
+            @Returns
+                depth: np.float32 array.
+        '''
+
+        try:
+            depth_16bit = cv2.imread(filename, -1)
+        except:
+            print('Load Error: {}'.format(filename))
+            sys.exit()
+
+        depth = np.float32(depth_16bit) / float(self.__bit16) * depth_max
+        return depth
+
+    def __load_image(self, filename, image_max=1.0):
+
+        try:
+            image_8bit = cv2.imread(filename, 1)
+        except:
+            print('Load Error: {}'.format(filename))
+            sys.exit()
+
+        image = np.float32(image_8bit) / float(self.__bit8) * image_max
+
+        return image
+
+    def __load_label(self, filename):
+
+        try:
+            label = cv2.imread(filename, 1)
+        except:
+            print('Load Error: {}'.format(filename))
+            sys.exit()
+
+        return np.float32(label)
 
 
-def load_label(filename):
+if __name__ == '__main__':
 
-    try:
-        label = cv2.imread(filename, 1)
-    except:
-        print('Load Error: {}'.format(filename))
-        sys.exit()
-
-    return label
-
+    d = ImageDataGenerator()
+    d.flow_from_directory(self, 'data', batch_size=32)
