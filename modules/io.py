ac90516cd744017eaa6f7c8d18e4d16dc872e82a
0000143145 <hajime.mihara@sony.com>
2018-03-17 18:32:06 +0900

    debug for loading

--- modules/io.py
+++ modules/io.py
@@ -13,13 +13,13 @@ import os
 import cv2
 import numpy as np
 
-from keras.preprocessing.image import load_img, img_to_array
+import matplotlib.pyplot as plt
 
 
 class ImageDataGenerator(object):
     def __init__(self):
         self.__bit16 = 65535
-        self.__bit8 = 65535
+        self.__bit8 = 255
 
         self.dirname = None
         self.reset()
@@ -38,18 +38,19 @@ class ImageDataGenerator(object):
                 os.path.join(self.dirname, state, 'color')
                 )
 
-            num_amples = len(image_names)
+            num_samples = len(image_names)
 
             # make [[color_name, depth_name], label_name] list
             data_path_list = []
             for image_name in image_names:
-                color_path = os.path.join(self.dirname, 'train', 'color')
-                depth_path = os.path.join(self.dirname, 'train', 'depth')
+                color_path = os.path.join(self.dirname, state, 'color', image_name)
+                depth_path = os.path.join(self.dirname, state, 'depth', image_name)
 
-                label_path = os.path.join(self.dirname, 'train', 'label')
+                label_path = os.path.join(self.dirname, state, 'label', image_name)
 
                 data_path_list.append(
                     ([color_path, depth_path], label_path))
+
 
             # random shuffle
             data_path_list = np.random.permutation(data_path_list)
@@ -62,9 +63,9 @@ class ImageDataGenerator(object):
                 depth_path = imagedepth_path[1]
 
                 image = self.__load_image(image_path)
-                depth = self.__load_image(depth_path)
+                depth = self.__load_depth(depth_path)
 
-                label = self.__load_image(label_path)
+                label = self.__load_label(label_path)
 
                 # make 4ch array (B, G, R, D)
                 x = np.dstack((image, depth))
@@ -86,20 +87,19 @@ class ImageDataGenerator(object):
                 depth: np.float32 array.
         '''
 
-        try:
-            depth_16bit = cv2.imread(filename, -1)
-        except:
+        depth_16bit = cv2.imread(filename, cv2.IMREAD_ANYDEPTH)
+        if depth_16bit is None:
             print('Load Error: {}'.format(filename))
             sys.exit()
 
         depth = np.float32(depth_16bit) / float(self.__bit16) * depth_max
+        print(depth.shape)
         return depth
 
     def __load_image(self, filename, image_max=1.0):
 
-        try:
-            image_8bit = cv2.imread(filename, 1)
-        except:
+        image_8bit = cv2.imread(filename, 1)
+        if image_8bit is None:
             print('Load Error: {}'.format(filename))
             sys.exit()
 
@@ -108,10 +108,9 @@ class ImageDataGenerator(object):
         return image
 
     def __load_label(self, filename):
+        label = cv2.imread(filename, 0)
 
-        try:
-            label = cv2.imread(filename, 1)
-        except:
+        if label is None:
             print('Load Error: {}'.format(filename))
             sys.exit()
 
@@ -121,4 +120,4 @@ class ImageDataGenerator(object):
 if __name__ == '__main__':
 
     d = ImageDataGenerator()
-    d.flow_from_directory(self, 'data', batch_size=32)
+    d.flow_from_directory('data')
