9d532ac1831e968f599199f89511ff6f4e95d140
0000143145 <hajime.mihara@sony.com>
2018-03-17 15:22:55 +0900

    add train.py

---
+++ train.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+"""
+@file       train.py
+@brief      excecutes training
+@author		mhr380
+@requirements   keras
+                tensorflow==1.4.0
+                opencv-python
+                numpy
+"""
+
+import keras
